ce3639ade4a952849b5d1d12d0edf4c2c2841c08
TurcanIvan <68420218+TurcanIvan@users.noreply.github.com>
2021-12-03 20:46:05 +0200

    HW

---
+++ for.basic.py
@@ -0,0 +1,18 @@
+# formula
+
+#for <variable> in <interable>:
+#   CODE_TO_REPEAT
+#HW1: for remake -> 10,9 ..2 1
+
+for n in range (10,0,-1):
+    print(n) 
+ 
+
+#for n in range (1,10):  # [1....10 11]
+    #print(n)
+#for n in 1000:
+#    print(n)
+#for c in "abcd":
+    #print(c)
+#for v in [10,20,30]:
+    #print(v)    
